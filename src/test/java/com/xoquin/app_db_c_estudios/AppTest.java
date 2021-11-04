0d6f326a736a23da52003cc6d1b00de0a1c3a874
xoko14 <xoquinperezb@gmail.com>
2021-11-04 20:42:47 +0100

    first commit

---
+++ src/test/java/com/xoquin/app_db_c_estudios/AppTest.java
@@ -0,0 +1,20 @@
+package com.xoquin.app_db_c_estudios;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
