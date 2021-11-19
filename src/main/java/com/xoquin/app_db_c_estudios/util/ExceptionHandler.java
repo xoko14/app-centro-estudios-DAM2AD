c4012a1184c69014970ec5a24076f4c10508f769
xoko14 <xoquinperezb@gmail.com>
2021-11-19 19:48:03 +0100

    ExceptionHandler documented

--- src/main/java/com/xoquin/app_db_c_estudios/util/ExceptionHandler.java
+++ src/main/java/com/xoquin/app_db_c_estudios/util/ExceptionHandler.java
@@ -2,7 +2,15 @@ package com.xoquin.app_db_c_estudios.util;
 
 import com.xoquin.app_db_c_estudios.factory.DialogFactory;
 
+/**
+ * Clase para xestionar excepcións. Só ten un método estático que lanzará un diálogo coa excepción trataada.
+ */
 public class ExceptionHandler {
+
+    /**
+     * Lanza un diálogo coa excepción tratada.
+     * @param e Excepción a amosar no diálogo.
+     */
     public static void handle(Exception e){
         DialogFactory df = new DialogFactory(DialogFactory.EXCEPTION_DIALOG);
         df.setException(e);
